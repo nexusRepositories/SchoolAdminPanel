94d0835383cf65c3479e47b3739f5a36abfc8cfb
nexusRepositories <golovatij1987@gmail.com>
2022-05-28 17:07:46 +0300

    rename title

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>SchoolADP</title>
+    <title>School Admin Panel</title>
     <meta charset="utf-8">
     <meta name="description" content="My Admin Panel">
     <meta name="viewport" content="width=device-width, initial-scale=1">
