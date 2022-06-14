2980bdd83632a571ecdb89d82a27aa9caa326bc2
nexusRepositories <golovatij1987@gmail.com>
2022-06-14 22:52:03 +0300

    HEADER: fix userpic & clear code

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -802,7 +802,7 @@
                 <div class="d-flex align-items-center ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
                     <!--begin::Menu wrapper-->
                     <div class="cursor-pointer" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end" style="width: 32px; height: 32px; border-radius: 30%; display: flex; flex-direction: row; justify-content: center; align-items: center; border: 1px solid lightgrey; object-fit: contain; overflow: hidden">
-                        <img src="{{ $user->url ? $user->url : asset('assets/user.svg') }}" alt="userpic" style="display: block; width: 100%; height: auto; max-height: 100%; max-width: 28px;">
+                        <img src="{{ $user->url ? asset($user->url) : asset('assets/user.svg') }}" alt="userpic" style="display: block; width: 100%; height: auto; max-height: 100%; max-width: 28px;">
                     </div>
                     <!--begin::User account menu-->
                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true" style="">
@@ -811,14 +811,12 @@
                             <div class="menu-content d-flex align-items-center px-3">
                                 @auth
                                     <div class="symbol symbol-50px me-5" style="width: 50px; height: 50px; border-radius: 15%; display: flex; flex-direction: row; justify-content: center; align-items: center; border: 1px solid lightgrey; object-fit: contain; overflow: hidden">
-                                        <img src="{{ $user->url ? $user->url : asset('assets/user.svg') }}" alt="userpic" style="display: block; width: 100%; height: auto; max-height: 100%; max-width: 48px;">
+                                        <img src="{{ $user->url ? asset($user->url) : asset('assets/user.svg') }}" alt="userpic" style="display: block; width: 100%; height: auto; max-height: 100%; max-width: 48px;">
                                     </div>
                                     <div class="d-flex flex-column">
                                         <div class="fw-bolder d-flex align-items-center fs-5">
                                             {{ $user->fullName() }} 
-                                            <span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2">Pro</span>
                                         </div>
-                                        <a href="#" class="fw-bold text-muted text-hover-primary fs-7">{{$user->email}}</a>
                                     </div>
                                 @endauth
                                 @guest
